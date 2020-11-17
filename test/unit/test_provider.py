1dbfc78f6a78fc16d2278bb88c2661ba5206ad33
Georg Kasper <gkasper@atlassian.com>
2020-11-16 21:28:37 -0800

    feat(utils): adds utils mod with Provider with tests

---
+++ test/unit/test_provider.py
@@ -0,0 +1,56 @@
+import logging
+from logging import Logger
+from unittest import TestCase
+
+from observe.lib.logger import Logger as CustomLogger
+from observe.lib.utils import Provider
+
+
+class TestProviderGetLogger(TestCase):
+
+    def test_when_args_are_falsy(self):
+        # arrange, act
+        logger = Provider.get_logger(None, object, {}, [])
+        # assert
+        self.assertIsInstance(logger, Logger)
+        self.assertEqual(logger.name, "Observe")
+
+    def test_when_args_have_logger(self):
+        # arrange, act
+        logger = Provider.get_logger(Logger("ArgLogger"))
+        # assert
+        self.assertIsInstance(logger, Logger)
+        self.assertEqual(logger.name, "ArgLogger")
+
+    def test_when_args_is_class_with_Logger(self):
+        # arrange
+        class A:
+            def __init__(self):
+                self.logger = Logger(name="Logger")
+        # act
+        logger = Provider.get_logger(*(A(),))
+        # assert
+        self.assertIsInstance(logger, Logger)
+        self.assertEqual(logger.name, "Logger")
+
+    def test_when_args_is_class_with_getLogger(self):
+        # arrange
+        class A:
+            def __init__(self):
+                self.logger = logging.getLogger("getLogger")
+        # act
+        logger = Provider.get_logger(*(0, A()))
+        # assert
+        self.assertIsInstance(logger, Logger)
+        self.assertEqual(logger.name, "getLogger")
+
+    def test_when_args_is_class_with_CustomLogger(self):
+        # arrange
+        class A:
+            def __init__(self):
+                self.logger = CustomLogger(name="CustomLogger")
+        # act
+        logger = Provider.get_logger(*(A(),))
+        # assert
+        self.assertIsInstance(logger, Logger)
+        self.assertEqual(logger.name, "CustomLogger")

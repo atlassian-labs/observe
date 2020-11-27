8ff8a323e156dc6fc8169c217a6e578d6144e7de
Georg Kasper <gkasper@atlassian.com>
2020-11-27 13:30:52 -0800

    feat(utils): adds Resolver class, resolver resolves tags_from, refactor logs

--- observe/lib/utils.py
+++ observe/lib/utils.py
@@ -1,7 +1,7 @@
 """This module provides utils for the @observe operator, to keep the actual implementation maintainable and readable.
 """
 import logging
-from typing import Any, Union
+from typing import Any, Dict, List, Optional, Union
 
 from datadog.dogstatsd.base import DogStatsd
 
@@ -43,8 +43,8 @@ class Provider:
         try:
             return Slack()
         except MissingSlackWebhookException:
-            Provider.get_logger().info(
-                "@observe: slack is disabled, add 'SLACK_WEB_HOOK' to os.environ in order to use.")
+            Provider.get_logger().debug(
+                "@observe: can't send notification to slack, add 'SLACK_WEB_HOOK' to os.environ to activate.")
 
     @staticmethod
     def get_metric(*args: Any) -> Union[IMetric, DogStatsd]:  # pylint: disable=E1136
@@ -62,3 +62,50 @@ class Provider:
                 return arg.statsd
 
         return Metric()
+
+
+class Resolver:
+
+    @staticmethod
+    def resolve_tags_from(tags_from: Optional[Dict[str, List[str]]], **kwargs: Any) -> List[str]:
+
+        # initialize default empty list
+        tags: List[str] = []
+
+        # ff15, we don't need to find additional tags
+        if not tags_from:
+            # return empty list
+            return tags
+
+        # iterate the tags from dictionary
+        for lookup_key, tags_keys in tags_from.items():
+
+            # ensure we work with types, we expected to get
+            if not isinstance(lookup_key, str) or not isinstance(tags_keys, list):
+                continue
+
+            # try to get the message from kwargs
+            message = kwargs.get(lookup_key)
+
+            # ensure the message was available, and is a dictionary
+            if not message or not isinstance(message, dict):
+                continue
+
+            # iterate all tags we supposed to find
+            for tag_key in tags_keys:
+
+                # ensure we work with types, we expected to get
+                if not isinstance(tag_key, str):
+                    continue
+
+                # check if the tag was in the message
+                tag_value = message.get(tag_key)
+
+                # ensure we did get a value and the value is a string
+                if not tag_value or not isinstance(tag_value, str):
+                    continue
+
+                # create the tag and append
+                tags.append("%s:%s" % (tag_key, tag_value))
+
+        return tags

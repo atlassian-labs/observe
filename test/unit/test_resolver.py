c7d4fb8818abf3ab7e7815a1bee7d9a9904c1498
Georg Kasper <gkasper@atlassian.com>
2020-11-27 14:15:30 -0800

    tests: adds additional tests

--- test/unit/test_resolver.py
+++ test/unit/test_resolver.py
@@ -72,6 +72,28 @@ class TestResolverTagsFromEmpty(TestCase):
         # assert
         self.assertEqual(tags, [])
 
+    def test_tags_from_returns_empty_list_on_message_not_dict(self):
+        # arrange kwargs
+        my_kwargs = {
+            "message": "type"
+        }
+        # act
+        tags = Resolver.resolve_tags_from(tags_from={"message": ["type", "schema"]}, **my_kwargs)
+        # assert
+        self.assertEqual(tags, [])
+
+    def test_tags_from_returns_empty_list_on_tag_not_string(self):
+        # arrange kwargs
+        my_kwargs = {
+            "message": {
+                "type": "easy_type"
+            }
+        }
+        # act
+        tags = Resolver.resolve_tags_from(tags_from={"message": [{"type"}]}, **my_kwargs)
+        # assert
+        self.assertEqual(tags, [])
+
 
 class TestResolverTagsFromFound(TestCase):
     """Defines tests for the Resolver.resolve_tags_from staticmethod, where tags were resolved
